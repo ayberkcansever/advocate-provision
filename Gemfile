e09fd6e39c87dcfb6facc4bcae7c6eca13345212
Tony Pitale <tpitale@gmail.com>
2017-02-18 23:37:39 -0600

    Delete chef stuff

--- Gemfile
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-source "https://rubygems.org"
-
-gem 'knife-solo'
-gem 'knife-solo_data_bag'
-gem 'chef'
-gem 'librarian-chef'
--- site-cookbooks/advocate-server/attributes/advocate-server.rb
+++
@@ -1,3 +0,0 @@
-default['advocate-server']['hostname'] = 'advocate'
-default['advocate-server']['user']['id'] = 'deploy'
-default['advocate-server']['database']['name'] = 'advocate_production'
